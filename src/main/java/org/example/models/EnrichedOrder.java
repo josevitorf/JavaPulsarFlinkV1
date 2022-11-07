9ce0c3acc31223a89bf8838270c3d53efa4ae1a5
josevitorf <josevitor.f@hotmail.com>
2022-11-07 16:07:22 -0300

    first commit

---
+++ src/main/java/org/example/models/EnrichedOrder.java
@@ -0,0 +1,17 @@
+package org.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnrichedOrder {
+    private long invoiceId;
+    private long lineItemId;
+    private User user;
+    private Item item;
+    private long createdAt;
+    private long paidAt;
+}
